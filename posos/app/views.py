6da02f3b7ecad0bdd941b1b5e032c369dbd2dc10
Lenfer <DeysonSH@gmail.com>
2017-11-08 14:39:38 +0300

    added test projects and create main page view

--- posos/app/views.py
+++ posos/app/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import Project
 
-# Create your views here.
+
+class AllProjectsView(TemplateView):
+    template_name = "app/all_projects_template.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(AllProjectsView, self).get_context_data()
+        context['projects'] = Project.objects.all()
+        return context
