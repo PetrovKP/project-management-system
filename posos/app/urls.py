7a5f0249f66e58f5131919874911b22252a840de
Lenfer <DeysonSH@gmail.com>
2017-11-08 14:39:04 +0300

    add auth system

---
+++ posos/app/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import url, include
+from .views import *
+
+
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+
+    url(r'^', include('django.contrib.auth.urls')),
+    url(r'^$', AllProjectsView.as_view(), name='index'),
+]
+
