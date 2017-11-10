927e09c78ed362913d6249c22b2106167ffdbba3
Lenfer <DeysonSH@gmail.com>
2017-11-10 14:20:33 +0300

    fix fix

--- posos/app/admin.py
+++ posos/app/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(TicketStatus);
-admin.site.register(ProjectStatus);
-admin.site.register(Project);
-admin.site.register(Ticket);
-
-# Register your models here.
+admin.site.register(TicketStatus)
+admin.site.register(ProjectStatus)
+admin.site.register(Project)
+admin.site.register(Ticket)
